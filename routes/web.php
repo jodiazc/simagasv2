19fc1d0acde1c950293d791e26e2918a65f4b61a
Jorge Díaz <jodiazc@gmail.com>
2024-10-01 14:02:37 -0600

    cambio por problemas de redireccionamientos

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return redirect('login');
+    return view('welcome');
+    //return redirect('login');
 });
 
 /*Route::middleware([
